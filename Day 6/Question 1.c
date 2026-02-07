d1b54a282484a8d9f05ab31fc961d8231fe24389
Ayushi Mishra <Ayushi.25314@stu.upes.ac.in>
2026-02-07 18:14:09 +0530

    Create Question 1.c

---
+++ Day 6/Question 1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    int arr[100005]; 
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    if(n <= 1) {
+     
+        for(int i = 0; i < n; i++) {
+            if(i > 0) printf(" ");
+            printf("%d", arr[i]);
+        }
+        printf("\n");
+        return 0;
+    }
+    
+    int write = 1;  
+    
+    for(int read = 1; read < n; read++) {
+        if(arr[read] != arr[write-1]) {
+            arr[write] = arr[read];
+            write++;
+        }
+    }
+    for(int i = 0; i < write; i++) {
+        if(i > 0) printf(" ");
+        printf("%d", arr[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
