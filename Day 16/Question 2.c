f10564dcee26b32221fa88129905ae897c8a993b
Ayushi Mishra <Ayushi.25314@stu.upes.ac.in>
2026-02-19 07:01:40 +0530

    Update Question 2.c

--- Day 16/Question 2.c
+++ Day 16/Question 2.c
@@ -1,1 +1,37 @@
+/*
+Given an integer array nums, find the subarray with the largest sum, and return its sum.
 
+ 
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Explanation: The subarray [1] has the largest sum 1.
+Example 3:
+
+Input: nums = [5,4,-1,7,8]
+Output: 23
+Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+*/
+int maxSubArray(int* nums, int numsSize) {
+    int max_sum = nums[0];       
+    int current_sum = nums[0];  
+    
+    for (int i = 1; i < numsSize; i++) {
+        current_sum = (current_sum + nums[i] > nums[i]) 
+                    ? current_sum + nums[i] 
+                    : nums[i];
+        
+        if (current_sum > max_sum) {
+            max_sum = current_sum;
+        }
+    }
+    
+    return max_sum;
+}
