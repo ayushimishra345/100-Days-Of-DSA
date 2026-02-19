6c9fc35af4832f88f1eb82e0bce40ac06006e8a1
Ayushi Mishra <Ayushi.25314@stu.upes.ac.in>
2026-02-19 06:57:52 +0530

    Update Question 2.c

--- Day 16 /Question 2.c
+++ Day 16 /Question 2.c
@@ -1,1 +1,41 @@
+/*
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
 
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+  */
+
+void rotate(int* nums, int numsSize, int k) {
+    k %= numsSize;
+    if (k == 0) return;
+    
+    int count = 0;
+    for (int start = 0; count < numsSize; start++) {
+        int current = start;
+        int prev = nums[start];
+        
+        do {
+            int next = (current + k) % numsSize;
+            int temp = nums[next];
+            nums[next] = prev;
+            prev = temp;
+            current = next;
+            count++;
+        } while (start != current);
+    }
+}
