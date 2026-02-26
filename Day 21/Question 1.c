3ad20f864fde30c74de058247e4aae6a780d34c4
Ayushi Mishra <Ayushi.25314@stu.upes.ac.in>
2026-02-26 06:35:12 +0530

    Update Question 1.c

--- Day 21/Question 1.c
+++ Day 21/Question 1.c
@@ -16,3 +16,64 @@ Input:
 Output:
 10 20 30 40 50
 */
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertAtEnd(struct Node** head, int value) {
+    struct Node* newNode = createNode(value);
+    
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    
+    struct Node* current = *head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    
+    current->next = newNode;
+}
+
+void printList(struct Node* head) {
+    struct Node* current = head;
+    
+    while (current != NULL) {
+        printf("%d ", current->data);
+        current = current->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    struct Node* head = NULL;  
+    for (int i = 0; i < n; i++) {
+        int value;
+        scanf("%d", &value);
+        insertAtEnd(&head, value);
+    }
+    printList(head);
+    
+    struct Node* current = head;
+    while (current != NULL) {
+        struct Node* temp = current;
+        current = current->next;
+        free(temp);
+    }
+    
+    return 0;
+}
