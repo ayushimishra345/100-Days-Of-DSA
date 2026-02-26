576536945b59d62bfc8934ab777825fe30172d2c
Ayushi Mishra <Ayushi.25314@stu.upes.ac.in>
2026-02-26 06:48:05 +0530

    Update Question 2.c

--- Day 24/Question 2.c
+++ Day 24/Question 2.c
@@ -1,1 +1,52 @@
 
+/*
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+ 
+
+Example 1:
+
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+Example 2:
+
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+Example 3:
+
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+*/
+
+
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    struct ListNode dummy;
+    dummy.next = head;
+    
+    struct ListNode* prev = &dummy;
+    struct ListNode* curr = head;
+    
+    while (curr != NULL) {
+        if (curr->val == val) {
+          
+            prev->next = curr->next;
+           
+            curr = prev->next;  
+        } else {
+            prev = curr;
+            curr = curr->next;
+        }
+    }
+    
+    return dummy.next;
+}
