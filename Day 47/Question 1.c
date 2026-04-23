f095681aa9e8f1b8d68206d81884978ce9097824
Ayushi Mishra <Ayushi.25314@stu.upes.ac.in>
2026-04-23 08:50:02 +0530

    Update Question 1.c

--- Day 47/Question 1.c
+++ Day 47/Question 1.c
@@ -1,1 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+// Node structure
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+// Create node
+struct Node* newNode(int val) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Build tree from level order
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1)
+        return NULL;
+
+    struct Node* root = newNode(arr[0]);
+
+    struct Node* queue[n];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+    int i = 1;
+
+    while (i < n && front < rear) {
+        struct Node* curr = queue[front++];
+
+        // Left child
+        if (i < n && arr[i] != -1) {
+            curr->left = newNode(arr[i]);
+            queue[rear++] = curr->left;
+        }
+        i++;
+
+        // Right child
+        if (i < n && arr[i] != -1) {
+            curr->right = newNode(arr[i]);
+            queue[rear++] = curr->right;
+        }
+        i++;
+    }
+
+    return root;
+}
+
+// Height function
+int height(struct Node* root) {
+    if (root == NULL)
+        return 0;
+
+    int left = height(root->left);
+    int right = height(root->right);
+
+    return 1 + (left > right ? left : right);
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int arr[N];
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    struct Node* root = buildTree(arr, N);
+
+    printf("%d", height(root));
+
+    return 0;
+}
