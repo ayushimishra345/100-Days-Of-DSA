81b7c0bfa4248e9b776626078b329080879d8a46
Ayushi Mishra <Ayushi.25314@stu.upes.ac.in>
2026-04-23 08:40:49 +0530

    Update Question 1.c

--- Day 26/Question 1.c
+++ Day 26/Question 1.c
@@ -1,1 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+// Define the structure for a doubly linked list node
+struct Node {
+    int data;
+    struct Node* prev;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert nodes sequentially at the end
+struct Node* insertNodes(int arr[], int n) {
+    struct Node* head = NULL;
+    struct Node* tail = NULL;
+
+    for (int i = 0; i < n; i++) {
+        struct Node* newNode = createNode(arr[i]);
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
+    return head;
+}
+
+// Function to traverse and print the list in forward order
+void traverseForward(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    struct Node* head = insertNodes(arr, n);
+    traverseForward(head);
+
+    return 0;
+}
