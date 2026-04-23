8831cb84e46ecf2bfd8a4220e13ed106523565e7
Ayushi Mishra <Ayushi.25314@stu.upes.ac.in>
2026-04-23 08:50:50 +0530

    Update Question 1.c

--- Day 49/Question 1.c
+++ Day 49/Question 1.c
@@ -1,1 +1,58 @@
 
+/*BST Insert*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+// Create node
+struct Node* newNode(int val) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Insert into BST
+struct Node* insert(struct Node* root, int val) {
+    if (root == NULL)
+        return newNode(val);
+
+    if (val < root->data)
+        root->left = insert(root->left, val);
+    else
+        root->right = insert(root->right, val);
+
+    return root;
+}
+
+// Inorder traversal
+void inorder(struct Node* root) {
+    if (root == NULL) return;
+
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    struct Node* root = NULL;
+
+    for (int i = 0; i < N; i++) {
+        int x;
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+
+    inorder(root);
+
+    return 0;
+}
