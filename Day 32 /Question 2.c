a5d33d0cd9b3d09d6965c9313e586563c6ce4a55
Ayushi Mishra <Ayushi.25314@stu.upes.ac.in>
2026-04-23 08:45:30 +0530

    Update Question 2.c

--- Day 32 /Question 2.c
+++ Day 32 /Question 2.c
@@ -1,1 +1,90 @@
 
+/*Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+
+ 
+
+Example 1:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+ 
+
+Constraints:
+
+-231 <= val <= 231 - 1
+Methods pop, top and getMin operations will always be called on non-empty stacks.
+At most 3 * 104 calls will be made to push, pop, top, and getMin.*/
+
+
+
+#include <stdlib.h>
+#include <limits.h>
+
+typedef struct {
+    int *stack;      // main stack
+    int *minStack;   // auxiliary stack to track minimums
+    int top;         // index for main stack
+    int capacity;    // maximum size
+} MinStack;
+
+MinStack* minStackCreate() {
+    MinStack* obj = (MinStack*)malloc(sizeof(MinStack));
+    obj->capacity = 30000; // as per constraints (max calls)
+    obj->stack = (int*)malloc(obj->capacity * sizeof(int));
+    obj->minStack = (int*)malloc(obj->capacity * sizeof(int));
+    obj->top = -1;
+    return obj;
+}
+
+void minStackPush(MinStack* obj, int val) {
+    obj->stack[++obj->top] = val;
+    if (obj->top == 0) {
+        obj->minStack[obj->top] = val;
+    } else {
+        int currentMin = obj->minStack[obj->top - 1];
+        obj->minStack[obj->top] = (val < currentMin) ? val : currentMin;
+    }
+}
+
+void minStackPop(MinStack* obj) {
+    if (obj->top >= 0) {
+        obj->top--;
+    }
+}
+
+int minStackTop(MinStack* obj) {
+    return obj->stack[obj->top];
+}
+
+int minStackGetMin(MinStack* obj) {
+    return obj->minStack[obj->top];
+}
+
+void minStackFree(MinStack* obj) {
+    free(obj->stack);
+    free(obj->minStack);
+    free(obj);
+}
