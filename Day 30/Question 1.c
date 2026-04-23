68c88160aadbf4f76643b51e644e8873aa51f99d
Ayushi Mishra <Ayushi.25314@stu.upes.ac.in>
2026-04-23 08:43:28 +0530

    Update Question 1.c

--- Day 30/Question 1.c
+++ Day 30/Question 1.c
@@ -1,1 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+// Node definition for polynomial term
+typedef struct Node {
+    int coeff;
+    int exp;
+    struct Node* next;
+} Node;
+
+// Function to create a new node
+Node* createNode(int coeff, int exp) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->coeff = coeff;
+    newNode->exp = exp;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert node at the end
+Node* insertNode(Node* head, int coeff, int exp) {
+    Node* newNode = createNode(coeff, exp);
+    if (!head) return newNode;
+
+    Node* temp = head;
+    while (temp->next) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    return head;
+}
+
+// Function to print polynomial
+void printPolynomial(Node* head) {
+    Node* temp = head;
+    while (temp) {
+        printf("%d", temp->coeff);
+        if (temp->exp > 0) {
+            printf("x");
+            if (temp->exp > 1) {
+                printf("^%d", temp->exp);
+            }
+        }
+        if (temp->next) {
+            printf(" + ");
+        }
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    Node* head = NULL;
+    for (int i = 0; i < n; i++) {
+        int coeff, exp;
+        scanf("%d %d", &coeff, &exp);
+        head = insertNode(head, coeff, exp);
+    }
+
+    printPolynomial(head);
+
+    return 0;
+}
